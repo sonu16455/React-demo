fc694b290b7aaa33a3ee6f29d6b956ec29c6293a
sonu16455 <sonu16455@gmail.com>
2018-03-22 00:50:56 +0530

    update

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "React";
+
+export default class Home extends React.Component{
+    render(){ 
+        return(
+                <h1>This Is from Home Component</h1>
+        );
+    }
+}
