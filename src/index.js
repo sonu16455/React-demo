b3d636eb9474c829d831c5c12288c9f2158021bb
sonu16455 <sonu16455@gmail.com>
2018-03-22 10:27:35 +0530

    update

--- src/index.js
+++ src/index.js
@@ -48,13 +48,13 @@ class Person extends React.Component{
 
                  <div class="container">
              
-                    <div class="da-slide">
+                    <div class="back">
                         <h2>Easy management</h2>
                         <h4>Easy to use</h4>
                         <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. 
                              Separated they live in Bookmarksgrove right at the coast of the Semantics, 
                              a large language ocean.</p>
-                        <button><a href="#" class="da-link button">Read more</a></button>
+                        <a href="#" class="da-link button">Read more</a>
 
                         <div class="da-img"><img src="../src/images/mobile.png" width="320" alt="image02"/></div>
                         </div>
